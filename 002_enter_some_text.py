ba86bae176516549523133f5135e6c34077e3d3a
486dx <hakanorcan@outlook.com>
2017-12-16 14:44:36 +0300

    First commit

---
+++ 002_enter_some_text.py
@@ -0,0 +1,3 @@
+text = input("Enter some text ")
+print("This is what you entered:")
+print(text)
---
+++ 003_enter_some_text _with_line.py
@@ -0,0 +1,3 @@
+text = input("enter some text\n")
+print ("This is what you entered:")
+print (text)
