e7542bec6b203fb8a8577bc4d7ea621b5d2a0963
486dx <hakanorcan@outlook.com>
2017-12-17 23:05:21 +0300

    new commit

--- 006_while_sample_01.py
+++ 006_while_sample_01.py
@@ -1,7 +1,7 @@
 total = 0.0
 count = 0
 while count < 3:
-    number = float(input("Enter a number: "))
+    number = float(input("Enter a number: " ))
     count = count + 1
     total =  total + number
 average = total / 3
---
+++ 007_print_sample_01.py
@@ -0,0 +1,31 @@
+print (2.0 + 4.0)
+print (6.0 - 4.0)
+print (6.0 * 3.0)
+print (6.0 / 3.0)
+print (6.0 % 3.0)
+print (6.0 // 3.0) # floor division: always truncates fractional remainders
+print (-5.0)
+print (3.0**2.0)   # three to the power of 2
+
+print (2 + 4)
+print (6 - 4)
+print (6 * 3)
+print (6 / 3)
+print (6 % 3)
+print (6 // 3) # floor division: always truncates fractional remainders
+print (-5)
+print (3**2)   # three to the power of 2
+
+
+# mixing data types in expressions
+# mixed type expressions are "converted up"
+# converted up means to take the data type with the greater storage
+# float has greater storage (8 bytes) than a regular int (4 bytes)
+print (2 + 4.0)
+print (6 - 4.0)
+print (6 * 3.0)
+print (6 / 3.0)
+print (6 % 3.0)
+print (6 // 3.0) # floor division: always truncates fractional remainders
+print (-5.0)
+print (3**2.0)   # three to the power of 2
