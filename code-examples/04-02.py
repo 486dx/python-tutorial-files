e041da9ab37a1c36fb1b935602cd4c705639d4ed
486dx <hakanorcan@outlook.com>
2017-12-27 11:47:24 +0300

    commit 27122017

--- code-examples/04-02.py
+++ code-examples/04-02.py
@@ -1,6 +1,5 @@
 #converting one data type to another
-
-number1 = input("Enter first number:\n")
+number1 = input("Enter the number:\n")
 print(number1, type(number1))
 number1 = int(number1)
 print(number1, type(number1))
